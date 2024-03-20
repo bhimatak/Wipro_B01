60280144184afc758c7c5791179738dec8230c17
bhimatak <bhima.t@gmail.com>
2024-03-20 10:52:17 +0530

    updated

--- classWork/day03/file1.c
+++ classWork/day03/file1.c
@@ -12,7 +12,7 @@ sy:
 			{
 				st3
 			}
-			st2	
+			st2
 		}
 		else
 		{
@@ -40,8 +40,8 @@ sy:
 		else
 		{
 			st8
-		}	
-		st9	
+		}
+		st9
 	}
 
 
@@ -54,29 +54,28 @@ sy:
 
 #include <stdio.h>
 
-
 int main()
 {
-	int relmart,dmart,kmart;
-	int open=0;
+	int relmart, dmart, kmart;
+	int open = 0;
 	int item = 0;
-	int available=0;
-
+	int available = 0;
+EndSt:
 	printf("\nrelmart is open: (0/1) ");
-	scanf("%d",&relmart);
-	if(relmart == 1)
+	scanf("%d", &relmart);
+	if (relmart == 1)
 		open = 1;
 	else
 		open = 0;
 	printf("\ndmart is open: (0/1) ");
-	scanf("%d",&dmart);
-	if(dmart == 1)
+	scanf("%d", &dmart);
+	if (dmart == 1)
 		open = 1;
 	else
 		open = 0;
 	printf("\nkmart is open: (0/1) ");
-	scanf("%d",&kmart);
-	if(kmart == 1)
+	scanf("%d", &kmart);
+	if (kmart == 1)
 		open = 1;
 	else
 		open = 0;
@@ -87,47 +86,45 @@ int main()
 	printf("\n2. Tea");
 	printf("\n3. Salt");
 	printf("\nChoice: ");
-	scanf("%d",&item);
+	scanf("%d", &item);
 
-	switch(item)
+	switch (item)
 	{
-		case 1:
-			printf("\nSugar is available");
-			available = 1;
-			break;
-		case 2:
-			printf("\nTea is available");
-			available = 1;
-			break;
-		case 3:
-			printf("\nSalt is available");
-			available = 1;
-			break;
-		default:
-			printf("\nitem out of range");
-			available = 0;
-			break;
+	case 1:
+		printf("\nSugar is available");
+		available = 1;
+		break;
+	case 2:
+		printf("\nTea is available");
+		available = 1;
+		break;
+	case 3:
+		printf("\nSalt is available");
+		available = 1;
+		break;
+	default:
+		printf("\nitem out of range");
+		available = 0;
+		break;
 	}
 
-	if(relmart)
+	if (relmart)
 	{
-		if((item ==1) && (available==1))
+		if ((item == 1) && (available == 1))
 		{
 
 			printf("\nBought Sugar");
-
+			goto EndSt;
 		}
-		else if((item ==2) && (available==1))
+		else if ((item == 2) && (available == 1))
 		{
 
 			printf("\nBought Tea");
-
 		}
-		else if((item ==3) && (available==1))
+		else if ((item == 3) && (available == 1))
 		{
 
 			printf("\nBought Salt");
-
 		}
 		else
 		{
@@ -135,27 +132,23 @@ int main()
 		}
 
 		printf(" from relmart\n");
-
 	}
-	else if(dmart)
+	else if (dmart)
 	{
-		if((item ==1) && (available==1))
+		if ((item == 1) && (available == 1))
 		{
 
 			printf("\nBought Sugar");
-
 		}
-		else if((item ==2) && (available==1))
+		else if ((item == 2) && (available == 1))
 		{
 
 			printf("\nBought Tea");
-
 		}
-		else if((item ==3) && (available==1))
+		else if ((item == 3) && (available == 1))
 		{
 
 			printf("\nBought Salt");
-
 		}
 		else
 		{
@@ -166,24 +159,21 @@ int main()
 	}
 	else
 	{
-		//kmart
-		if((item ==1) && (available==1))
+		// kmart
+		if ((item == 1) && (available == 1))
 		{
 
 			printf("\nBought Sugar");
-
 		}
-		else if((item ==2) && (available==1))
+		else if ((item == 2) && (available == 1))
 		{
 
 			printf("\nBought Tea");
-
 		}
-		else if((item ==3) && (available==1))
+		else if ((item == 3) && (available == 1))
 		{
 
 			printf("\nBought Salt");
-
 		}
 		else
 		{
