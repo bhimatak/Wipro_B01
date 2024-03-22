28bcaf29eb44c47ec86e6d077365fd885a8b2f20
bhimatak <bhima.t@gmail.com>
2024-03-22 15:42:07 +0530

    updated

--- classWork/day05/file6.c
+++ classWork/day05/file6.c
@@ -1,12 +1,18 @@
 #include <stdio.h>
 
 int dispList(int [], int);
+int sortDesc(int [], int);
+
 
 int main()
 {
     int arr[] = {10,1,3,5,2,8,9,22};
     int Size = sizeof(arr)/sizeof(arr[0]);
+    printf("\nBefore Sort\n");
     dispList(arr,Size);
+    sortDesc(arr,Size);
+    printf("\nAfter Sort");
+    dispList(arr, Size);
     printf("\n%d",arr[3]);
     printf("\n\n");
     return 0;
@@ -17,6 +23,9 @@ int dispList(int a[], int n)
     int i;
     for(i=0;i<n;i++)
         printf("%d->",a[i]);
-    a[3] = 101;
+    
     printf("\n");
 }
+
+
+// sort the elements of an array in desc order
